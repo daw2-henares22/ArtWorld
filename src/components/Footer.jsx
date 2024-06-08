0489a9f67df716db358d751473ecd25b0660e9ed
daw2-henares22 <henareshidalgoruben@fpllefia.com>
2024-06-08 20:17:57 +0200

    intentoImagenVercel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,18 @@
+import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { FaGithub } from 'react-icons/fa';
 
 export function Footer() {
-    const { t } = useTranslation(); // Obtener la función de traducción t()
-  
-    return (
-      <footer className="bg-blue-gray-900 dark:bg-white py-4 mt-8">
-        <div className="flex container mx-auto text-white dark:text-blue-gray-800">
-          <div className='pe-4 text-start'>{t('Created by')} Rubén Henares Hidalgo</div>
-          <button
-            href="https://github.com/daw2-henares22/ArtWorld"
-            target="_blank"
-            rel="noopener noreferrer"
-            className=" text-end text-white dark:text-blue-gray-800 dark:hover:text-blue-gray-400 hover:text-blue-gray-400 hover:scale-x-105 hover:scale-y-105 transition duration-150 shadow-lg hover:shadow-xl hover:shadow-white/50 shadow-white/50 dark:hover:shadow-blue-gray-800/50 dark:shadow-blue-gray-800/50">
-            GitHub
-          </button>
-        </div>
-      </footer>
-    );
-  }
+  const { t } = useTranslation();
+
+  return (
+    <footer className="bg-blue-gray-900 dark:bg-white py-4 mt-8">
+      <div className="flex justify-between items-center container mx-auto text-white dark:text-blue-gray-800">
+        <div className='text-start'>{t('Created by')} Rubén Henares Hidalgo</div>
+        <a href="https://github.com/daw2-henares22/ArtWorld" target="_blank" rel="noopener noreferrer">
+          <FaGithub className='w-6 h-6 text-white dark:text-blue-gray-800' />
+        </a>
+      </div>
+    </footer>
+  );
+}
