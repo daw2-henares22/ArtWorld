b3ea0ffad7235199c96144dc694882e3958a318b
daw2-henares22 <henareshidalgoruben@fpllefia.com>
2024-06-19 08:10:43 +0200

    optimizar

--- src/views/Users.jsx
+++ src/views/Users.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useGlobalContext } from '../context/globalContext';
 import { useNavigate } from 'react-router-dom';
 import { supabase, supabaseService } from '../bd/supaBase';
